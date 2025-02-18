39f5a101f61c776e6d43300f0d61b6540aacccc1
MaheshJay00 <87064191+MaheshJay00@users.noreply.github.com>
2025-02-18 06:13:21 -0500

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/query/"
+API_URL = "UR_URL"
 
 st.set_page_config(page_title="RAG-powered business docs Q&A",layout="wide")
 
